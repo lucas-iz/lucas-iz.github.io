43c92272832abeb0ae031c5df44d3359ad42fe5d
lucas-iz <lhahn98@gmail.com>
2025-05-01 14:40:05 +0200

    added geolocation

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Test");
+// Check if geolocation is available in the browser
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            console.log("Latitude:", position.coords.latitude);
+            console.log("Longitude:", position.coords.longitude);
+        },
+        (error) => {
+            console.error("Error getting location:", error.message);
+        }
+    );
+} else {
+    console.error("Geolocation is not supported by this browser.");
+}
