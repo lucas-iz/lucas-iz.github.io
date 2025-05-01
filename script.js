42ea45cb2078e1e4c52c9188705868ccb88be424
lucas-iz <lhahn98@gmail.com>
2025-05-01 14:52:21 +0200

    Added position to span's

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ if ("geolocation" in navigator) {
         (position) => {
             console.log("Latitude:", position.coords.latitude);
             console.log("Longitude:", position.coords.longitude);
+
+            const latDiv = document.getElementById("lat");
+            const lngDiv = document.getElementById("lng");
+
+            latDiv.textContent = position.coords.latitude;
+            lngDiv.textContent = position.coords.longitude;
         },
         (error) => {
             console.error("Error getting location:", error.message);
